c00fc523af78e10c5c2ae7ee017e9b0d49bb28b8
SOMNATH0904 <somnathshaw900@gmail.com>
2024-03-15 20:03:32 +0530

    Updated the ECommerce Website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import LoginSignUp from './Pages/LoginSignUp';
 import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
 
 function App() {
   return (
@@ -14,15 +18,16 @@ function App() {
         <Navbar></Navbar>
         <Routes>
           <Route path='/' element={<Shop></Shop>}></Route>
-          <Route path='/mens' element={<ShopCategory category="men" />}></Route>
-          <Route path='/womens' element={<ShopCategory category="women" />}></Route>
-          <Route path='/kids' element={<ShopCategory category="kid" />}></Route>
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />}></Route>
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />}></Route>
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />}></Route>
           <Route path='product' element={<Product></Product>}>
               <Route path=':productId' element={<Product></Product>}></Route>
           </Route>
           <Route path='/cart' element={<Cart></Cart>}></Route>
           <Route path='/login' element={<LoginSignUp></LoginSignUp>}></Route>
         </Routes>
+        <Footer></Footer>
       </BrowserRouter>
 
     </div>
